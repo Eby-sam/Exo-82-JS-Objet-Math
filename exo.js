bd17deb3999243c945cd44a832cd02020b133095
coque <coquelet.samuel@gmail.com>
2021-11-25 15:12:36 +0100

    fini

---
+++ exo.js
@@ -0,0 +1,53 @@
+let exo1 = Math.floor( 34.589456);
+console.log("exo 1 : " + exo1);
+
+let exo2 = Math.floor(68.412784);
+console.log("exo 2 : " + exo2);
+
+let exo3 = Math.ceil(39.456);
+console.log("exo 3 "+ exo3);
+
+let exo4 = Math.ceil(27.34876541);
+console.log("exo 4 : " + exo4);
+
+let exo5 = Math.round(5.68);
+console.log("exo 5 : " + exo5);
+
+let exo6 = Math.round(5.41);
+console.log("exo 6 : " + exo6);
+
+let exo7 = Math.trunc(6.12457);
+console.log("exo 7 : " + exo7);
+
+let exo8 = Math.trunc(9.568124);
+console.log("exo 8 : " + exo8);
+
+let exo9 = Math.min(5, 1, 12, -1, 45);
+console.log("exo9 : " + exo9);
+
+let a1 = 9.568124;
+console.log(Number.parseInt(a1) + " = 9.568124");
+
+function mini (nombre1, nombre2, nombre3, nombre4, nombre5){
+
+    return nombre1, nombre2, nombre3, nombre4, nombre5;
+}
+
+function max (nbr1 , nbr2 , nbr3 , nbr4, nbr5){
+
+    return nbr1, nbr2, nbr3, nbr4, nbr5;
+}
+console.log(Math.min(mini(5,1,12,-1,45)));
+
+console.log(Math.min(mini(9,12,16,44,3)));
+
+console.log(Math.max(max(5,1,12,-1,45)));
+
+console.log(Math.max(max(9,12,16,44,3)));
+
+let a = -6;
+console.log(Math.abs(a) + " = -6");
+
+let y = -56;
+console.log(Math.abs(y) + " = -56");
+
